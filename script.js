1442f7f81419044f056173427e21453b5b78195a
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-09 23:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+const players = [/* array elements */];
+const person = {/* object properties */};
 
-const person = {
-  name: "John Doe",
-  age: 80,
-};
-
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+const team = players;
+const team1 = [...players];
+const cap1 = {...person};
