afb28deea92a04cecab8f61673fcd4baf4b1756e
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-09 23:05:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+const players = ["John", "Bob", "Alice", "Poppy"];
+
+const person = {
+  name: "John Doe",
+  age: 80,
+};
 const players = [/* array elements */];
 const person = {/* object properties */};
 
 const team = players;
 const team1 = [...players];
 const cap1 = {...person};
+
+
+
+
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
+
