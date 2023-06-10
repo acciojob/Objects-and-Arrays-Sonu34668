1fcc2d4f1853280934a60d10ed7275d8aa348acd
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-10 21:39:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ let cap1 = {
 
 console.log(team);
 console.log(players);
-//console.log(team1);
-//console.log(cap1);
-
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
 window.person = person;
